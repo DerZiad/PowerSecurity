3a129320800561b9e49ac4444baee46a156b3ae3
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2022-04-17 19:00:48 +0000

    Work splitted

---
+++ PowerSecurityServer/src/main/java/io/powersecurity/PowerSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package io.powersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerSecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerSecurityServerApplication.class, args);
+	}
+
+}
---
+++ PowerSecurityServer/src/test/java/io/powersecurity/PowerSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.powersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerSecurityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

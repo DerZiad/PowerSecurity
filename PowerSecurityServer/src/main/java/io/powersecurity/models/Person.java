e741f5a56149a1ade7c8daf6634b99dc81e2985b
Ziad Bougrine <40337316+DerZiad@users.noreply.github.com>
2022-04-20 23:12:19 +0000

    Fixed

---
+++ PowerSecurityServer/src/main/java/io/powersecurity/models/Person.java
@@ -0,0 +1,38 @@
+package io.powersecurity.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Generated;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="persons")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "person")
+	private List<Picture> picture = new ArrayList<>();
+	
+	
+}
---
+++ PowerSecurityServer/src/main/java/io/powersecurity/models/Picture.java
@@ -0,0 +1,46 @@
+package io.powersecurity.models;
+
+import java.io.IOException;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sun.jdi.InvalidTypeException;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="pictures")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Picture {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Lob
+	private byte[] picture;
+	
+	private String fileName;
+	
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.REFRESH})
+	private Person person;
+	
+	public void setMultipartFile(MultipartFile file,String type) throws IOException,InvalidTypeException {
+		this.picture = file.getBytes();
+		this.fileName = this.picture.hashCode() + ".jpg";
+
+	}
+}
